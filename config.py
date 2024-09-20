15a2ce5a583c7abe098c400c7b573fdc2b540ea8
mandaringole07 <mandaringole2001@gmail.com>
2024-09-21 00:57:23 +0530

    Configuration File

---
+++ config.py
@@ -0,0 +1,7 @@
+FLASK_PORT_NUMBER=8090
+
+MODEL_FILE_NAME=r"artifacts\Std_Perf_Pred.pkl"
+
+MODEL_COLUMNS_DATA=r"artifacts\Std_Perf_Pred_Columns_data.json"
+
+CSV_File_Name=r"data\Student_performance_Data.csv"
