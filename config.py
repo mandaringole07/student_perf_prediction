1f5a7eb8512848b39f279cbad854fd8d4801ef51
mandaringole07 <mandaringole2001@gmail.com>
2024-09-21 10:20:53 +0530

    Configuration file

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
+import os
 FLASK_PORT_NUMBER=8090
 
-MODEL_FILE_NAME=r"artifacts\Std_Perf_Pred.pkl"
+MODEL_FILE_NAME=os.path.join("artifacts/Std_Perf_Pred.pkl")
 
-MODEL_COLUMNS_DATA=r"artifacts\Std_Perf_Pred_Columns_data.json"
+MODEL_COLUMNS_DATA=os.path.join("artifacts/Std_Perf_Pred_Columns_data.json")
 
-CSV_File_Name=r"data\Student_performance_Data.csv"
+#CSV_File_Name=os.path.join("data/Student_performance_Data.csv")
